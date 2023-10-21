31d7ee1b2c80f583688fba8d3eeb32d40efda2e7
Dingchao Zhang <dingchaozhang@Dingchaos-MBP.attlocal.net>
2023-10-21 11:02:48 -0500

    scroll and mantle commit reveal smart contract deployment

---
+++ Multi-chain-CommitReveal/scripts/deploy.js
@@ -0,0 +1,24 @@
+const hre = require("hardhat");
+
+async function main() {
+  
+  // Deploy the CommitReveal contract
+  const commitReveal = await hre.ethers.deployContract("CommitReveal",{
+    gasLimit: "0x1000000",
+  });
+  await commitReveal.waitForDeployment();
+
+
+  console.log("CommitReveal contract deployed to:", commitReveal.target);
+  // await hre.run("verify:verify", {
+  //   address:commitReveal.target,
+  //   contract: "contracts/CommitReveal.sol:CommitReveal",
+  //   constructorArguments:[],
+  // })
+}
+
+// Handle errors
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
